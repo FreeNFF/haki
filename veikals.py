328adc1824ecab013ffb3a7778799bc50a9b7350
FreeNFF <nauisfricsons30@gmail.com>
2024-12-05 13:39:58 +0200

    Update veikals.py

--- veikals.py
+++ veikals.py
@@ -1,2 +1,11 @@
 import requests
 
+url = 'https://fakestoreapi.com/products/category/jewelery'
+
+try:
+    response= requests.get(url)#sūta GET pieprasījumu
+    response.raise_for_status()#pārbauda
+    data = response.json()#ja pieprasijums ir veiksmigs tad trurpinas
+    print(data)
+except requests.exceptions.RequestException as e:
+    print(f"Error fetching data: {e}")
